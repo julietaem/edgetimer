5fabb8e49f62c40bb90cdcaadf36461a0d2e97c3
Julieta Escobar Múnera <julietaescobarm@gmail.com>
2026-05-16 11:57:38 -0500

    Arreglar CORS de Vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: 'https://edge-timer-blue.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
